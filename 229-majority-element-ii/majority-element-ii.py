e0d26d81bd647599174a7fd7e777fc3269e2b5dd
Ankush Rana <rajputx000@gmail.com>
2026-09-07 22:31:49 +0530

    Time: 5 ms (75.66%) | Memory: 22.2 MB (93.42%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        cand1, cand2 =None, None
+        count1, count2=0, 0
+        for num in nums:
+            if num==cand1:
+                count1+=1
+            elif num==cand2:
+                count2+=1
+            elif count1==0:
+                cand1,count1=num,1
+            elif count2==0:
+                cand2,count2=num,1
+            else:
+                count1-=1
+                count2-=1        
+        threshold = len(nums) // 3
+        result=[]
+        for cand in (cand1,cand2):
+            if cand is not None and nums.count(cand) > threshold:
+                result.append(cand)
+
+        return result
