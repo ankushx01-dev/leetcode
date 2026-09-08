0bac7158ad68571d0943d63f393dddf768fe02c9
Ankush Rana <rajputx000@gmail.com>
2026-09-08 16:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (10.14%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -11,9 +11,9 @@ class Solution:
         odd=head
         even_head=even
         while even is not None and even.next is not None:
-            odd.next=odd.next.next
+            odd.next=even.next
             odd=odd.next
-            even.next=even.next.next
+            even.next=odd.next
             even=even.next
         odd.next=even_head
         return head
